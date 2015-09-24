bedee1b167c758d5176ea6a131eebe14545895ed
Carl-Henrik Persson <ch.perssonn@gmail.com>
2015-09-24 10:29:43 +0200

    Create application.java

---
+++ application.java
@@ -0,0 +1,17 @@
+package com.example.main;
+
+import java.io.IOException;
+
+
+
+
+public class application {
+	
+	
+	public static void main(String[] args) throws IOException{
+		
+		server server = new server();
+		
+		
+	}
+}
